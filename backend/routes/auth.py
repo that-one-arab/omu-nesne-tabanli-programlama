6b1a1122b171aeb19ed6b9fefce58a09589b35ae
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2024-05-19 15:28:50 +0300

    Add auth routes

---
+++ backend/routes/auth.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, jsonify, request
+from app import db, bcrypt
+from models import User
+from flask_jwt_extended import create_access_token
+
+
+auth_blueprint = Blueprint("quizzing", __name__, url_prefix="/api")
+
+
+@auth_blueprint.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    if (
+        User.query.filter_by(username=username).first()
+        or User.query.filter_by(email=email).first()
+    ):
+        return jsonify({"message": "User already exists"}), 400
+
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+    new_user = User(username=username, email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User created successfully"}), 201
+
+
+@auth_blueprint.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(
+            identity={"id": user.id, "username": user.username, "email": user.email}
+        )
+        return jsonify({"access_token": access_token}), 200
+
+    return jsonify({"message": "Invalid credentials"}), 401
