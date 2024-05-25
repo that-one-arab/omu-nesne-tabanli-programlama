8af6b62fbbf47a6ed1d2067f28b8368222b651a5
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2024-05-25 18:30:12 +0300

    Add snackbar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { ThemeProvider } from "@emotion/react";
 import theme from "@/util/theme";
+import Snackbar from "@/components/Snackbar";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <Snackbar />
     </ThemeProvider>
   );
 }
